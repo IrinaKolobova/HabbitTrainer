398e0c79e3fa9f5066fe47381642ac3605b19e53
Irina Kolobova <kolobovairinav@gmail.com>
2020-08-28 22:28:15 -0700

    Show error message to the user

--- app/src/main/java/com/ivk/habbittrainer/CreateHabitActivity.kt
+++ app/src/main/java/com/ivk/habbittrainer/CreateHabitActivity.kt
@@ -9,6 +9,7 @@ import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
 import android.view.View
+import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_create_habit.*
 import java.io.IOException
 
@@ -18,9 +19,31 @@ class CreateHabitActivity : AppCompatActivity() {
 
     private val CHOOSE_IMAGE_REQUEST = 1
 
+    private var imageBitmap: Bitmap? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_habit)
+    }
+
+    fun storeHabit(view: View) {
+        if (et_title.isBlank() || et_description.isBlank()) {
+            Log.d(TAG, "No habit stored: title or description missing.")
+            displayErrorMessage("Your habit needs an engaging title and/or description")
+            return
+        } else if (imageBitmap == null) {
+            Log.d(TAG, "No habit stored: image was missing.")
+            displayErrorMessage("Add a motivating picture to your habit")
+            return
+        }
+
+        // Store the habit
+        tv_error.visibility = View.INVISIBLE
+    }
+
+    private fun displayErrorMessage(message: String) {
+        tv_error.text = message
+        tv_error.visibility = View.VISIBLE
     }
 
     fun chooseImage(view: View) {
@@ -31,7 +54,7 @@ class CreateHabitActivity : AppCompatActivity() {
         val chooser = Intent.createChooser(intent, "Choose image for habit")
         startActivityForResult(chooser, CHOOSE_IMAGE_REQUEST)
 
-        Log.d(TAG, "Intent to choose image was sent")
+        Log.d(TAG, "Intent to choose image was sent.")
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -39,13 +62,14 @@ class CreateHabitActivity : AppCompatActivity() {
 
         if (requestCode == CHOOSE_IMAGE_REQUEST && resultCode == Activity.RESULT_OK
             && data != null && data.data != null) {
-            Log.d(TAG, "An image was chosen by the user")
+            Log.d(TAG, "An image was chosen by the user.")
 
             val bitmap = tryReadBitmap(data.data!!)
 
             bitmap.let {
+                this.imageBitmap = bitmap
                 iv_image.setImageBitmap(bitmap)
-                Log.d(TAG, "Read image bitmap and updated image view")
+                Log.d(TAG, "Read image bitmap and updated image view.")
             }
         }
     }
@@ -59,3 +83,6 @@ class CreateHabitActivity : AppCompatActivity() {
         }
     }
 }
+
+// extension function
+private fun EditText.isBlank() = this.text.toString().isBlank()
