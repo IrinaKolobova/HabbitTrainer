dbf89de59415375350050af40539a48602329c2e
Irina Kolobova <kolobovairinav@gmail.com>
2020-08-28 20:31:25 -0700

    Use Intents to switch Activities

---
+++ app/src/main/java/com/ivk/habbittrainer/CreateHabitActivity.kt
@@ -0,0 +1,11 @@
+package com.ivk.habbittrainer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateHabitActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_habit)
+    }
+}
--- app/src/main/java/com/ivk/habbittrainer/MainActivity.kt
+++ app/src/main/java/com/ivk/habbittrainer/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.ivk.habbittrainer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -23,5 +25,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.main_menu, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.add_habit) {
+            switchTo(CreateHabitActivity::class.java)
+        }
+        return true
+    }
+
+    private fun switchTo(c: Class<*>) {
+        val intent = Intent(this, c)
+        startActivity(intent)
     }
 }
