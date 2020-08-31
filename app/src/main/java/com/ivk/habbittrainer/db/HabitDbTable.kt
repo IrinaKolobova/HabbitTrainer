49804539eff63aa3f1eda0871cd3eec0e4e46d61
Irina Kolobova <kolobovairinav@gmail.com>
2020-08-31 13:35:25 -0700

    Implement secure transactions using Kotlin extension functions

--- app/src/main/java/com/ivk/habbittrainer/db/HabitDbTable.kt
+++ app/src/main/java/com/ivk/habbittrainer/db/HabitDbTable.kt
@@ -2,6 +2,7 @@ package com.ivk.habbittrainer.db
 
 import android.content.ContentValues
 import android.content.Context
+import android.database.sqlite.SQLiteDatabase
 import android.graphics.Bitmap
 import android.util.Log
 import com.ivk.habbittrainer.Habit
@@ -21,9 +22,20 @@ class HabitDbTable(context: Context) {
         values.put(HabitEntry.DESCR_COL, habit.description)
         values.put(HabitEntry.IMAGE_COL, toByteArray(habit.image))
 
-        val id = db.insert(HabitEntry.TABLE_NAME, null, values)
+        /*db.beginTransaction()
+        val id = try {
+            val returnValue = db.insert(HabitEntry.TABLE_NAME, null, values)
+            db.setTransactionSuccessful()
 
-        db.close()
+            returnValue
+        } finally {
+            db.endTransaction()
+        }
+        db.close()*/
+
+        val id = db.transaction {
+            insert(HabitEntry.TABLE_NAME, null, values)
+        }
 
         Log.d(TAG, "Stored new habit to the DB $habit")
 
@@ -35,4 +47,19 @@ class HabitDbTable(context: Context) {
         bitmap.compress(Bitmap.CompressFormat.PNG, 0, stream)
         return stream.toByteArray()
     }
+}
+
+private inline fun <T> SQLiteDatabase.transaction(function: SQLiteDatabase.() -> T): T {
+    beginTransaction()
+    val result = try {
+        val returnValue = function()
+        setTransactionSuccessful()
+
+        returnValue
+    } finally {
+        endTransaction()
+    }
+    close()
+
+    return result
 }
