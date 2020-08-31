70b1ebc8ee2fb8943020e7e7625bd7e9da6a1840
Irina Kolobova <kolobovairinav@gmail.com>
2020-08-31 12:41:31 -0700

    Implement the Database Helper

--- app/src/main/java/com/ivk/habbittrainer/db/Contracts.kt
+++ app/src/main/java/com/ivk/habbittrainer/db/Contracts.kt
@@ -7,6 +7,7 @@ const val DATABASE_VERSION = 10
 
 object HabitEntry : BaseColumns {
     const val TABLE_NAME = "habit"
+    const val _ID = "id"
     const val TITLE_COL = "title"
     const val DESCR_COL = "description"
     const val IMAGE_COL = "image"
