0ff0e468f154c45a646ebc175afbe8e39fdd5fef
Irina Kolobova <kolobovairinav@gmail.com>
2020-08-31 12:30:14 -0700

    Define the Contracts

---
+++ app/src/main/java/com/ivk/habbittrainer/db/Contracts.kt
@@ -0,0 +1,13 @@
+package com.ivk.habbittrainer.db
+
+import android.provider.BaseColumns
+
+const val DATABASE_NAME = "habbittrainer.db"
+const val DATABASE_VERSION = 10
+
+object HabitEntry : BaseColumns {
+    const val TABLE_NAME = "habit"
+    const val TITLE_COL = "title"
+    const val DESCR_COL = "description"
+    const val IMAGE_COL = "image"
+}
